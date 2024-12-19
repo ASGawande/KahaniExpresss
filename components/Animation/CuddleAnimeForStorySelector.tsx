1d9008383c100be930a84649b7df412ba7d6e5f3
ASGawande <sagargawande843@gmail.com>
2024-12-18 22:57:13 -0800

    Fixed issue cuddle was having issue moving to right

--- components/Animation/CuddleAnimeForStorySelector.tsx
+++ components/Animation/CuddleAnimeForStorySelector.tsx
@@ -8,28 +8,31 @@ interface CuddleAnimeForStorySelectorProps {
   animationName: string;
 }
 
-const CuddleAnimeForStorySelector: React.FC<CuddleAnimeForStorySelectorProps> = ({ animationName = 'Idle' }) => {
+const CuddleAnimeForStorySelector: React.FC<CuddleAnimeForStorySelectorProps> = ({
+  animationName = 'Idle',
+}) => {
   return (
     <View style={styles.container}>
+      {/* Canvas for 3D rendering */}
       <Canvas
         camera={{ position: [0, 2, 5], fov: 50 }}
         style={{ flex: 1 }} // Ensure the canvas fills the container
       >
-        {/* Add children for Canvas */}
-        <>
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
-          <Cuddles animationName={animationName} />
-          <hemisphereLight intensity={0.3} groundColor={0x444444} />
-          <OrbitControls
-            enableZoom={false}
-            enableRotate={true}
-            enablePan={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Environment preset="sunset"/>
-        </>
+        {/* 3D Scene */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
+        <Cuddles animationName={animationName} />
+        {/* Placeholder mesh to verify the canvas */}
+        
+        <hemisphereLight intensity={0.3} groundColor={0x444444} />
+        <OrbitControls
+          enableZoom={false}
+          enableRotate={true}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Environment preset="sunset" />
       </Canvas>
     </View>
   );
@@ -37,8 +40,9 @@ const CuddleAnimeForStorySelector: React.FC<CuddleAnimeForStorySelectorProps> = 
 
 const styles = StyleSheet.create({
   container: {
+    
     height: 500,
-    width: '100%',
+    width: 500,
   },
 });
 
--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,9 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('glb');
 
+// Add 'three' to the assetExts and sourceExts
+config.resolver.assetExts.push('bin');
+
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
 module.exports = config;
